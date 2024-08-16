000fac88908031902e325fb1d77d72186271878c
DO YOUNG KIM <dlsrksrlaehdud@naver.com>
2024-08-16 12:58:41 +0900

    Initial Commit

---
+++ src/main/java/SPOilerBackend/SpOilerBackendApplication.java
@@ -0,0 +1,13 @@
+package SPOilerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpOilerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpOilerBackendApplication.class, args);
+	}
+
+}
